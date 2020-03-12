b6b71142ade3665ec844c3f46f31b255e958abc9
CasacoVermelho <alexandrebmjr@gmail.com>
2020-03-12 00:43:50 -0300

    ultimas alterações da aula que eu lembro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Display = props =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{backgroundColor:"#dedede",border:"1px solid"}}>
+        <h1>{props.number}</h1>
+      </div>
+    );
+}
+
+const App = () =>{
+  return (
+    <Display number={10}/>
   );
 }
 
