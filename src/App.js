4213ccb1107407c7369446328b935897debdaa6d
CasacoVermelho <alexandrebmjr@gmail.com>
2020-03-13 22:02:40 -0300

    finalizada montagem da parte visual

--- src/App.js
+++ src/App.js
@@ -3,15 +3,49 @@ import './App.css';
 
 const Display = props =>{
   return (
-      <div style={{backgroundColor:"#dedede",border:"1px solid"}}>
-        <h1>{props.number}</h1>
+      <div style={{padding:10,textAlign:"right",backgroundColor:"#eeeeee",border:"1px solid"}}>
+        <h1>{props.show}</h1>
       </div>
     );
 }
 
+const Teclado = () =>{
+	return (
+		<div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-between"}}>
+			<Botao digit={7}/>
+			<Botao digit={8}/>
+			<Botao digit={9}/>
+			<Botao digit={"/"}/>
+			<Botao digit={4}/>
+			<Botao digit={5}/>
+			<Botao digit={6}/>
+			<Botao digit={"*"}/>
+			<Botao digit={1}/>
+			<Botao digit={2}/>
+			<Botao digit={3}/>
+			<Botao digit={"-"}/>
+			<Botao digit={0}/>
+			<Botao digit={"."}/>
+			<Botao digit={"="}/>
+			<Botao digit={"+"}/>
+		</div>
+	);
+}
+
+const Botao = props =>{
+	return (
+		<div style={{marginTop:"5px",borderRadius:"30px",textAlign:"center",width:"23%",backgroundColor:"#cccccc",border:"1px solid"}}>
+			<h2>{props.digit}</h2>
+		</div>
+	);
+}
+
 const App = () =>{
   return (
-    <Display number={10}/>
+  	<div style={{backgroundColor:"#777777",width:"100%",maxWidth:"350px",margin:"auto"}}>
+    	<Display show={0}/>
+    	<Teclado/>
+    </div>
   );
 }
 
